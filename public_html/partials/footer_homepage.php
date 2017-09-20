fc50310034e611325ccd2c299f2c68cc3c5f3eb4
heyharuna <heyharuna0221@gmail.com>
2017-09-20 15:50:13 -0700

    finish footer homepage on desktop

--- public_html/partials/footer_homepage.php
+++ public_html/partials/footer_homepage.php
@@ -6,11 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="../css/main.css">
 	<link rel="stylesheet" href="../css/bootstrap-grid.css">
 	<link rel="stylesheet" href="../css/bootstrap-grid.min.css">
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" href="../css/bootstrap.css">
-	<link rel="stylesheet" href="../css/main.css">
+
 	<!-- Google font  -->
 	<link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab" rel="stylesheet">
 	</head>
---
+++ public_html/partials/footer_innerpage.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="../css/bootstrap-grid.css">
+	<link rel="stylesheet" href="../css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/main.css">
+	<!-- Google font  -->
+	<link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab" rel="stylesheet">
+	</head>
+	<body>
+        <!-- <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                      1 of 3
+                    </div>
+                    <div class="col">
+                      2 of 3
+                    </div>
+                    <div class="col">
+                      3 of 3
+                    </div>
+                </div>
+            </div>
+        </footer> -->
+        <div class="container-fruid">
+            <!----------- Footer ------------>
+            <footer class="footer-bs innerpage-footer">
+                <div class="row">
+                	<div class="col-md-3 footer-brand animated fadeInLeft">
+						<div class="footer-logo media">
+							<a  href="index.php" class="d-flex mr-3">
+								<img src="../img/logo/logo_black.svg" width="50" height="50" class="d-inline-block align-top" alt="logo">
+							</a>
+						</div>
+                        <p>© 2014 BS3 UI Kit, All rights reserved</p>
+                    </div>
+                	<div class="col-md-4 footer-nav animated fadeInUp">
+                    	<h4>Menu —</h4>
+                    	<div class="col-md-6">
+                            <ul class="pages">
+                                <li><a href="#">Travel</a></li>
+                                <li><a href="#">Nature</a></li>
+                                <li><a href="#">Explores</a></li>
+                                <li><a href="#">Science</a></li>
+                                <li><a href="#">Advice</a></li>
+                            </ul>
+                        </div>
+                    	<div class="col-md-6">
+                            <ul class="list">
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Contacts</a></li>
+                                <li><a href="#">Terms & Condition</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                	<div class="col-md-2 footer-social animated fadeInDown">
+                    	<h4>Follow Us</h4>
+                    	<ul>
+                        	<li><a href="#">Facebook</a></li>
+                        	<li><a href="#">Twitter</a></li>
+                        	<li><a href="#">Instagram</a></li>
+                        	<li><a href="#">RSS</a></li>
+                        </ul>
+                    </div>
+                	<div class="col-md-3 footer-ns animated fadeInRight">
+                    	<h4>Newsletter</h4>
+                        <p>A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>
+                        <p>
+                            <div class="input-group">
+                              <input type="text" class="form-control" placeholder="Search for...">
+                              <span class="input-group-btn">
+                                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-envelope"></span></button>
+                              </span>
+                            </div><!-- /input-group -->
+                         </p>
+                    </div>
+                </div>
+            </footer>
+            <section style="text-align:center; margin:10px auto;"><p>Designed by <a href="http://enfoplus.net">Prince J. Sargbah</a></p></section>
+
+        </div>
+	</body>
+</html>
