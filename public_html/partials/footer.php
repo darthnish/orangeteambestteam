51749ee9e0040d993f702d9dcada2516d65c2428
heyharuna <heyharuna0221@gmail.com>
2017-09-20 00:23:39 -0700

    upload fotter file of homepage

--- public_html/partials/footer.php
+++ public_html/partials/footer.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+	<head>
+	<!-- Required meta tags -->
 	<meta charset="utf-8">
-	<title></title>
-</head>
-<body>
-	
-</body>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="../css/bootstrap-grid.css">
+	<link rel="stylesheet" href="../css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/main.css">
+	<!-- Google font  -->
+	<link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab" rel="stylesheet">
+	</head>
+	<body>
+		<footer class="homepage_footer">
+			<nav class="navbar navbar-light bg-light">
+			 	<a class="navbar-brand" href="#">
+			    	<img src="../img/sns/facebook.svg" width="50" height="50" class="d-inline-block align-top" alt="facebook">
+				</a>
+				<a class="navbar-brand" href="#">
+			    	<img src="../img/sns/twitter_white.svg" width="50" height="50" class="d-inline-block align-top" alt="twitter">
+				</a>
+				<a class="navbar-brand" href="#">
+			    	<img src="../img/sns/instagram_white.svg" width="50" height="50" class="d-inline-block align-top" alt="instagram">
+				</a>
+			</nav>
+		</footer>
+	</body>
 </html>
