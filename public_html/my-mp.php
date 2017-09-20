2f5a33e0e9e12d193ca7822e0898d30c1e85d396
sach8m <arcocokaco@gmail.com>
2017-09-20 15:33:57 -0700

    offset don't work

--- public_html/my-mp.php
+++ public_html/my-mp.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Who's my MP</title>
-	<!-- Google fonts-->
-	<link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab|Zilla+Slab+Highlight" rel="stylesheet">
-	<!-- CSS -->
-	<link rel="stylesheet" href="css/mp.css">
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Who's my MP</title>
+		<!-- Google fonts-->
+		<link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab|Zilla+Slab+Highlight" rel="stylesheet">
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/mp.css">
+		<!-- Bootstrap CSS-->
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/bootstrap-grid.css">
+	</head>
 	<body>
 		<div class="row">
 			<div class="col-xs-12">
@@ -16,7 +19,7 @@
 		</div>
 		
 		<div class="row">
-			<div class="col-xs-12 col-md-offset-3 col-md-3">
+			<div class="col-xs-12 col-md-4 offset-md-4">
 				<h1 class="sec-title">Select Your</h1>
 				<select name="province" id="province">
 					<option value="Province">Province</option>
@@ -30,14 +33,25 @@
 					<option value="Manitoba">Manitoba</option>
 					<option value="New Brunswick">New Brunswick</option>
 					<option value="Prince Edward Island">Prince Edward Island</option>
-				</select>
-				<p>Click on the map and see your Member of Parlement</p>
+				</select>				
+				<p class="text">Click on the map and see your Member of Parlement</p>
 			</div>
 		</div>		
 
 		<!--Modal Box-->
-		<div class="">
-			
+		<div class="row">
+			<div class="col-xs-12 col-md-4 offset-md-4">
+				<h1 class="title-modal">Current MP in</h1>				
+			</div>
+			<div class="offset-md-4 col-md-4">
+				<select name="parties" id="parties">
+					<option value="all parties">All Parties</option>
+					<option value="Lireral">Lireral</option>
+					<option value="NDP">NDP</option>
+					<option value="Conservative">Conservative</option>
+					<option value="Green">Green</option>
+				</select>			
+			</div>			
 		</div>
 	</body>
 </html>
