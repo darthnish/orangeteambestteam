7c5e5a23d0eff94149c170e44687680b9347d701
sach8m <arcocokaco@gmail.com>
2017-09-19 18:03:49 -0700

    start coding mp page

--- public_html/my-mp.php
+++ public_html/my-mp.php
@@ -2,9 +2,42 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title></title>
+	<title>Who's my MP</title>
+	<!-- Google fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab|Zilla+Slab+Highlight" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/mp.css">
 </head>
-<body>
-	
-</body>
+	<body>
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class="title">Who's My MP</h1>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-xs-12 col-md-offset-3 col-md-3">
+				<h1 class="sec-title">Select Your</h1>
+				<select name="province" id="province">
+					<option value="Province">Province</option>
+					<option value="Ontario">Ontario</option>
+					<option value="British Columbia">British Columbia</option>
+					<option value="Quebec">Quebec</option>
+					<option value="Alberta">Alberta</option>
+					<option value="Nova Scoria">Nova Scoria</option>
+					<option value="Saskatchewan">Saskatchewan</option>
+					<option value="Newfoundland and Labrador">Newfoundland and Labrador</option>
+					<option value="Manitoba">Manitoba</option>
+					<option value="New Brunswick">New Brunswick</option>
+					<option value="Prince Edward Island">Prince Edward Island</option>
+				</select>
+				<p>Click on the map and see your Member of Parlement</p>
+			</div>
+		</div>		
+
+		<!--Modal Box-->
+		<div class="">
+			
+		</div>
+	</body>
 </html>
