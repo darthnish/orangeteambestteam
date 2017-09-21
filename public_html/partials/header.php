9e34f3fec541a9992c2277209187eba007781094
heyharuna <heyharuna0221@gmail.com>
2017-09-20 17:22:20 -0700

    finish header section

--- public_html/partials/header.php
+++ public_html/partials/header.php
@@ -1,63 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../css/main.css">
-    <link rel="stylesheet" href="../css/bootstrap-grid.css">
-    <link rel="stylesheet" href="../css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <!-- Google font  -->
-    <link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab" rel="stylesheet">
-  </head>
-  <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			 <a class="navbar-brand" href="#">
-	      		<img src="../img/logo/logo_white.svg" width="140" height="65" alt="img">
-	    	</a>
-	    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-		     <ul class="navbar-nav">
-                 <li class="nav-item dropdown nav-li">
- 		         	<a class="nav-link dropdown-toggle nav-a nav-party text-white" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PARTY</a>
- 		         	<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
- 						<a class="dropdown-item nav-a nav-a-img" href="liberal.php">
-                             <img src="../img/logo/liberal_logo.svg" width="120" height="40" alt="liberalParty">
-                        </a>
-                        <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
-                             <img src="../img/logo/conservative_logo.png" width="120" height="auto" alt="liberalParty">
-                        </a>
-                        <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
-                             <img src="../img/logo/ndp_logo.svg" width="120" height="40" alt="liberalParty">
-                        </a>
-                        <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
-                             <img src="../img/logo/green_logo.svg" width="120" height="50" alt="liberalParty">
-                        </a>
- 		         	</div>
- 				</li>
-				 <li class="nav-item active  nav-li">
-		         	<a class="nav-link nav-a text-white" href="#">WHO'S MY MP <span class="sr-only">(current)</span></a>
-		        </li>
-		        <li class="nav-item nav-compare  nav-li">
-		         	<a class="nav-link nav-a text-white" href="#">COMPARE PARTY</a>
-		        </li>
-		        <li class="nav-item  nav-li">
-                    <a class="navbar-brand" href="#">
-       	      		        <img src="../img/icon/mail_white.png" width="50" height="40" alt="img">
-       	    	   </a>
-		        </li>
-		    </ul>
-		</div>
-	</nav>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="../js/bootstrap.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-  </body>
-</html>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		 <a class="navbar-brand" href="#">
+      		<img src="../img/logo/logo_white.svg" width="140" height="65" alt="img">
+    	</a>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+	     <ul class="navbar-nav">
+             <li class="nav-item dropdown nav-li">
+		         	<a class="nav-link dropdown-toggle nav-a nav-party text-white" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PARTY</a>
+		         	<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item nav-a nav-a-img" href="liberal.php">
+                         <img src="../img/logo/liberal_logo.svg" width="120" height="40" alt="liberalParty">
+                    </a>
+                    <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
+                         <img src="../img/logo/conservative_logo.png" width="120" height="auto" alt="liberalParty">
+                    </a>
+                    <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
+                         <img src="../img/logo/ndp_logo.svg" width="120" height="40" alt="liberalParty">
+                    </a>
+                    <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
+                         <img src="../img/logo/green_logo.svg" width="120" height="50" alt="liberalParty">
+                    </a>
+		         	</div>
+				</li>
+			 <li class="nav-item active  nav-li">
+	         	<a class="nav-link nav-a text-white" href="#">WHO'S MY MP <span class="sr-only">(current)</span></a>
+	        </li>
+	        <li class="nav-item nav-compare  nav-li">
+	         	<a class="nav-link nav-a text-white" href="#">COMPARE PARTY</a>
+	        </li>
+	        <li class="nav-item  nav-li">
+                <a class="navbar-brand" href="#">
+   	      		        <img src="../img/icon/mail_white.png" width="50" height="40" alt="img">
+   	    	   </a>
+	        </li>
+	    </ul>
+	</div>
+</nav>
