7569764be6521ef61a792f1a1cbd9778c92bd764
heyharuna <heyharuna0221@gmail.com>
2017-09-20 17:20:01 -0700

    finish footer in homepage

--- public_html/partials/footer_homepage.php
+++ public_html/partials/footer_homepage.php
@@ -1,33 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="../css/main.css">
-	<link rel="stylesheet" href="../css/bootstrap-grid.css">
-	<link rel="stylesheet" href="../css/bootstrap-grid.min.css">
-	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/bootstrap.css">
-
-	<!-- Google font  -->
-	<link href="https://fonts.googleapis.com/css?family=Lato|Zilla+Slab" rel="stylesheet">
-	</head>
-	<body>
-		<footer class="homepage_footer">
-			<nav class="navbar navbar-light bg-light">
-			 	<a class="navbar-brand" href="#">
-			    	<img src="../img/sns/facebook.svg" width="50" height="50" class="d-inline-block align-top" alt="facebook">
-				</a>
-				<a class="navbar-brand" href="#">
-			    	<img src="../img/sns/twitter_white.svg" width="50" height="50" class="d-inline-block align-top" alt="twitter">
-				</a>
-				<a class="navbar-brand" href="#">
-			    	<img src="../img/sns/instagram_white.svg" width="50" height="50" class="d-inline-block align-top" alt="instagram">
-				</a>
-			</nav>
-		</footer>
-	</body>
-</html>
+	<footer class="homepage_footer">
+		<nav class="navbar navbar-light bg-light">
+		 	<a class="navbar-brand" href="#">
+		    	<img src="../img/sns/facebook.svg" width="50" height="50" class="d-inline-block align-top" alt="facebook">
+			</a>
+			<a class="navbar-brand" href="#">
+		    	<img src="../img/sns/twitter_white.svg" width="50" height="50" class="d-inline-block align-top" alt="twitter">
+			</a>
+			<a class="navbar-brand" href="#">
+		    	<img src="../img/sns/instagram_white.svg" width="50" height="50" class="d-inline-block align-top" alt="instagram">
+			</a>
+		</nav>
+	</footer>
