64823dc13d915ffb1ad2c26dea72eaf5c40a8cd0
heyharuna <heyharuna0221@gmail.com>
2017-09-20 17:39:07 -0700

    upload URL in homepage header

--- public_html/partials/footer_homepage.php
+++ public_html/partials/footer_homepage.php
@@ -1,13 +1,13 @@
 
 	<footer class="homepage_footer">
 		<nav class="navbar navbar-light bg-light">
-		 	<a class="navbar-brand" href="#">
+		 	<a class="navbar-brand" href="facebook.com">
 		    	<img src="../img/sns/facebook.svg" width="50" height="50" class="d-inline-block align-top" alt="facebook">
 			</a>
-			<a class="navbar-brand" href="#">
+			<a class="navbar-brand" href="twitter.com">
 		    	<img src="../img/sns/twitter_white.svg" width="50" height="50" class="d-inline-block align-top" alt="twitter">
 			</a>
-			<a class="navbar-brand" href="#">
+			<a class="navbar-brand" href="instagram.com">
 		    	<img src="../img/sns/instagram_white.svg" width="50" height="50" class="d-inline-block align-top" alt="instagram">
 			</a>
 		</nav>
