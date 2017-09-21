c952b9dac177ab3645d5b2141ef5c47f4c5d62fb
heyharuna <heyharuna0221@gmail.com>
2017-09-20 19:52:26 -0700

    update css in header and footer section

--- public_html/partials/header.php
+++ public_html/partials/header.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 		 <a class="navbar-brand" href="#">
-      		<img src="../img/logo/logo_white.svg" width="140" height="65" alt="img">
+      		<img src="img/logo/logo_white.svg" width="140" height="65" alt="img">
     	</a>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
 	     <ul class="navbar-nav">
@@ -9,16 +9,16 @@
 		         	<a class="nav-link dropdown-toggle nav-a nav-party text-white" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PARTY</a>
 		         	<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 						<a class="dropdown-item nav-a nav-a-img" href="liberal.php">
-                         <img src="../img/logo/liberal_logo.svg" width="120" height="40" alt="liberalParty">
+                         <img src="img/logo/liberal_logo.svg" width="120" height="40" alt="liberalParty">
                     </a>
                     <a class="dropdown-item nav-a nav-a-img" href="conservative.php">
-                         <img src="../img/logo/conservative_logo.png" width="120" height="auto" alt="liberalParty">
+                         <img src="img/logo/conservative_logo.png" width="120" height="auto" alt="liberalParty">
                     </a>
                     <a class="dropdown-item nav-a nav-a-img" href="ndp.php">
-                         <img src="../img/logo/ndp_logo.svg" width="120" height="40" alt="liberalParty">
+                         <img src="img/logo/ndp_logo.svg" width="120" height="40" alt="liberalParty">
                     </a>
                     <a class="dropdown-item nav-a nav-a-img" href="green.php">
-                         <img src="../img/logo/green_logo.svg" width="120" height="50" alt="liberalParty">
+                         <img src="img/logo/green_logo.svg" width="120" height="50" alt="liberalParty">
                     </a>
 		         	</div>
 				</li>
@@ -30,7 +30,7 @@
 	        </li>
 	        <li class="nav-item  nav-li">
                 <a class="navbar-brand" href="#">
-   	      		        <img src="../img/icon/mail_white.png" width="50" height="40" alt="img">
+   	      		        <img src="img/icon/mail_white.png" width="50" height="40" alt="img">
    	    	   </a>
 	        </li>
 	    </ul>
