9c0ffd9707a3a10d007aaf56db1651986386bffc
heyharuna <heyharuna0221@gmail.com>
2017-09-20 17:25:44 -0700

    add url in header section

--- public_html/partials/header.php
+++ public_html/partials/header.php
@@ -11,22 +11,22 @@
 						<a class="dropdown-item nav-a nav-a-img" href="liberal.php">
                          <img src="../img/logo/liberal_logo.svg" width="120" height="40" alt="liberalParty">
                     </a>
-                    <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
+                    <a class="dropdown-item nav-a nav-a-img" href="conservative.php">
                          <img src="../img/logo/conservative_logo.png" width="120" height="auto" alt="liberalParty">
                     </a>
-                    <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
+                    <a class="dropdown-item nav-a nav-a-img" href="ndp.php">
                          <img src="../img/logo/ndp_logo.svg" width="120" height="40" alt="liberalParty">
                     </a>
-                    <a class="dropdown-item nav-a nav-a-img" href="liberal.php">
+                    <a class="dropdown-item nav-a nav-a-img" href="green.php">
                          <img src="../img/logo/green_logo.svg" width="120" height="50" alt="liberalParty">
                     </a>
 		         	</div>
 				</li>
 			 <li class="nav-item active  nav-li">
-	         	<a class="nav-link nav-a text-white" href="#">WHO'S MY MP <span class="sr-only">(current)</span></a>
+	         	<a class="nav-link nav-a text-white" href="my-mp.php">WHO'S MY MP <span class="sr-only">(current)</span></a>
 	        </li>
 	        <li class="nav-item nav-compare  nav-li">
-	         	<a class="nav-link nav-a text-white" href="#">COMPARE PARTY</a>
+	         	<a class="nav-link nav-a text-white" href="compare-parties.php">COMPARE PARTY</a>
 	        </li>
 	        <li class="nav-item  nav-li">
                 <a class="navbar-brand" href="#">
