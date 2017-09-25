ea8f3c4b90cd5fb4557b473c6529f282e63677a5
Peter Korompis <peter.paschalk@gmail.com>
2017-09-25 11:33:08 -0700

    Added Back Mp-Page

--- public_html/my-mp.php
+++ public_html/my-mp.php
@@ -5,43 +5,24 @@
 		<title>Who's my MP</title>
 		<!-- Google fonts-->
 		<link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,900|Zilla+Slab:400,500,500i,600,700" rel="stylesheet">
-		<!-- Custom CSS -->
+		<!-- CSS -->
 		<link rel="stylesheet" href="css/mp.css">
 		<link rel="stylesheet" href="css/header.css">
 		<link rel="stylesheet" href="css/footer.css">
-		<!-- Bootstrap CSS-->
 		<link rel="stylesheet" href="css/bootstrap.css">
 		<link rel="stylesheet" href="css/bootstrap-grid.css">
 		<link rel="stylesheet" href="css/bootstrap-reboot.css">
-		<!-- Bootstrap JS-->
-		<link rel="stylesheet" href="js/bootstrap.js">
-		<link rel="stylesheet" href="js/main.js">	
 	</head>
 
-	<?php include 'partials/header_2.php' ?>
-
 	<body>
-		<div class="row">
-			<div class="offset-xs-1 col-xs-11">
-				<h1 class="title">Who's My MP</h1>
-			</div>
-		</div>
-		<div class="container">
-  <div class="row">
-    <!-- <div class="col align-self-start">
-      One of three columns
-    </div> -->
-    <div class="col align-self-center">
-      One of three columns
-    </div>
-    <div class="col align-self-end">
-      One of three columns
-    </div>
-  </div>
-</div>
-		<div class="container">
-			<div class="row">
-				<div class="col align-self-center">
+	
+		
+		<div class="container-fluid container-fluid--mp">
+
+			<h1 class="title">Who's My MP</h1>
+
+			<div class="mp-select">
+				<div class="d-flex justify-content-center">
 					<h1 class="sec-title">Select Your</h1>
 					<select name="province" id="province">
 						<option value="Province">Province</option>
@@ -56,34 +37,66 @@
 						<option value="New Brunswick">New Brunswick</option>
 						<option value="Prince Edward Island">Prince Edward Island</option>
 					</select>
-				</div>
-			</div>
-		</div>
-		<div class="container">
-			<div class="row">
-				<div class="col align-self-center">
+				</div>	
+				<div class="d-flex justify-content-center">	
 					<p class="text">Click on the map and see your Member of Parlement</p>
 				</div>
 			</div>
+
+			<?php include 'partials/map.html' ?>
+
 		</div>
-		
+		<!-- End of container-fluid -->
 
-		<?php include 'partials/footer.php' ?>	
-
+		<?php include 'partials/footer.php' ?>
+			
+		<div class="container-fluid container-fluid--mp">
 		<!--Modal Box-->
-		<div class="row">
-			<div class="">
-				<h1 class="title-modal">Current MP in</h1>				
+
+		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#mp-modal">
+		  Launch demo modal
+		</button>
+
+		<!-- Modal -->
+		<div class="modal fade" id="mp-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-lg" role="document">
+		    <div class="modal-content">
+		      	<div class="modal-header">
+			        <h5 class="modal-title" id="exampleModalLabel"></h5>
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+			        </button>
+		      	</div>
+		      	<div class="modal-body">
+		        	<div class="container-fluid" id="test" data-party="page">
+						<div class="title-modal-con">
+							<h1 class="title-modal">Current MP in <span>Province</span></h1>	
+							<div class="modal-select">
+								<select name="parties" id="parties">
+									<option value="all parties">All Parties</option>
+									<option value="Liberal">Liberal</option>
+									<option value="NDP">NDP</option>
+									<option value="Conservative">Conservative</option>
+									<option value="Green Party">Green Party</option>
+								</select>			
+							</div>
+						</div>
+					<div id="display-info" class="row"></div>
+			    </div>
+			    <div class="modal-footer">
+				    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				    <button type="button" class="btn btn-primary">Save changes</button>
+			    </div>
 			</div>
-			<div class="">
-				<select name="parties" id="parties">
-					<option value="all parties">All Parties</option>
-					<option value="Lireral">Lireral</option>
-					<option value="NDP">NDP</option>
-					<option value="Conservative">Conservative</option>
-					<option value="Green">Green</option>
-				</select>			
-			</div>			
 		</div>
+
+		</div>
+		<!-- End of container-fluid -->
+
+		<!-- JS -->		
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>	
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/xml.js"</script>
+		<script src="js/mp.js"></script>
 	</body>
 </html>
