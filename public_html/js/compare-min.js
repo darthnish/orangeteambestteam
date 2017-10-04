2b6eaa8580b488f5d4ebce00b02a8aa2e8197f20
Peter Korompis <peter.paschalk@gmail.com>
2017-10-03 21:08:51 -0700

    Added Description

---
+++ public_html/js/compare-min.js
@@ -0,0 +1,37 @@
+//Enables Smooth Scrolling
+$(document).scroll(function() {
+    var yaxis = $(document).scrollTop(),
+        sidebar = $("#sidebar");
+    if(yaxis >= 120)  {
+        sidebar.addClass("fixed-bar");
+    } else {
+        sidebar.removeClass("fixed-bar");
+    }
+});
+
+//ADD TOOL TIP
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip(false)
+});
+
+//Enables Smooth Scrolling
+$(document).ready(function(){
+    $('.flex-icons a[href^="#"], .solid-button').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+//Enables Modal
+$('#results-modal').on('shown.bs.modal', function () {
+
+});
+
